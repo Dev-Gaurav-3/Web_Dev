5630ac1264aea7861e4eb10c20fb33fcc436277c
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2025-09-02 13:21:48 +0530

    add

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
 // variables //
 
-var a = 5;
+// var a = 5;
 
-var s = "Hello";  // can change
-const c = true;  // cannot change it
-console.log(a);
-console.log(s);
+// var s = "Hello";  // can change
+// const c = true;  // cannot change it
+// console.log(a);
+// console.log(s+a);
+
+// let meaning = {
+//     "hello" : "Hi",
+//     "Goodbye" : "Bye"
+// }
+
+// console.log(meaning);
+
+// var age = 18;
+
+// if(age>=18){
+//     console.log("You can Vote");
+// }
+// else{
+//     console.log("You cannot Vote");
+// }
+
+// // == -> only compares value , === -> compare value and type both //
+
+// if("3" == 3) console.log(true); // RETURN TRUE
+// if("3" === 3) console.log(true); // RETURN FALSE
+
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);    
+// }
+// var output = "";
+// for(let i = 0;i<10;i++){
+//     output += i + " "; 
+// }
+// console.log(output);
+
+// let dict = {
+//     "name" : "Gaurav",
+//     "course" : "Btech",
+//     "lang" : "C++"
+// }
+// for(var key in dict){
+//     const ele = dict[key];
+//     console.log(ele,key);
+// }
+
+// i = 5;
+
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// let num = 10;
+// do {
+//     console.log(num);
+//     num++;
+// } while (num<5);
+
+// FUNCTIONS //
+
+// function sum(a,b,c = 3){
+//     return a+b + c;
+// }
+// console.log(sum(5,6));
+// console.log(sum(5,6,5));
