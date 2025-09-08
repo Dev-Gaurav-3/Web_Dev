5ba9aa79439a4b2f0b6146216005f58a65c51183
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2025-09-08 20:56:26 +0530

    add

--- callback.js
+++ callback.js
@@ -29,16 +29,58 @@
 
 // PROMISE // 
 
-let prom1 = new Promise((resolve,reject) => {
+// let prom1 = new Promise((resolve,reject) => {
 
-    setTimeout(() => {
-        console.log("I am Done");
-        resolve("Gaur");  
-    }, 3000);
-})
+//     setTimeout(() => {
+//         console.log("I am Done");
+//         resolve("Gaur");  
+//     }, 3000);
+// })
 
-prom1.then((a) => {
-    console.log(a);
-}).catch((err)=>{
-    console.log("Error");
-})
+// prom1.then((a) => {
+//     console.log(a);
+// }).catch((err)=>{
+//     console.log("Error");
+// })
+
+// function getData(){
+
+//     return new Promise((resolve,reject) =>{
+//         setTimeout(() => {
+//             resolve(455);
+//         }, 3500);
+//     })
+// }
+
+// console.log("1st");
+// console.log("2nd");
+// console.log("3rd");
+
+// let data = getData();
+
+// data.then((v) => {            // But this is also callback approach 
+//     console.log(data);
+//     console.log("4th");
+
+// })
+
+// ASYNC AWAIT // 
+
+async function getdata() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(455);
+        }, 3500);
+    })
+}
+
+async function main() {
+    console.log("1st");
+    console.log("2nd");
+    console.log("3rd");
+    let data = await getdata(); // wait till getdata() gets executed //
+    console.log(data);
+    console.log("4th");
+}
+main();
+
