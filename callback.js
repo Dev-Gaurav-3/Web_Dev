4ad6383b0a38f44ea432f0534eb0e63409b1beec
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2025-09-11 18:18:32 +0530

    add

--- callback.js
+++ callback.js
@@ -84,39 +84,86 @@
 // }
 // main();
 
-let a = prompt("Enter a num: "); // by defaut String //
-let b = prompt("Enter a num: ");
-// what if user gives the wrong input //
+// let a = prompt("Enter a num: "); // by defaut String //
+// let b = prompt("Enter a num: ");
+// // what if user gives the wrong input //
 
-if(isNaN(a) || isNaN(b)){
-    throw SyntaxError("Sorry this is not allowed");
+// if(isNaN(a) || isNaN(b)){
+//     throw SyntaxError("Sorry this is not allowed");
+// }
+
+// let sum = parseInt(a) + parseInt(b);
+// console.log("The sum is :" , sum)
+// // console.log("The sum is :" , sum*x) // here x is not defined so how to catch it //
+
+// try {
+//     console.log("The sum is:", sum*x);
+// } catch (error) {
+//     console.log("Error aa gaya")
+// }
+
+// // like we have function and we are retunning some value and we want to execute something after return statement //
+
+// function main(){
+
+//     let x = 2;
+//     try {
+//         console.log("The sum is:", sum*x);
+//         return true;
+//     } catch (error) {
+//         console.log("Error aa gaya")
+//         return false;
+//     }
+//     finally{
+//         console.log("Executed !! ");
+//     }
+// }
+
+// main();
+
+// we get promise from APIs and then we do .. //
+
+// function asynFunc(){
+//     return new Promise((resolve,reject) =>{
+//         setTimeout(() => {
+//         console.log("Some data 1");
+//         // resolve("sucess");
+//         reject("Some Error")
+//     }, 3000);
+//     })
+// }
+
+// let p1 = asynFunc();
+// p1.then((res) =>{
+//     console.log(res);
+// })
+// p1.catch((res) =>{
+//     console.log(res);
+// })
+
+function asynFunc1(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+        console.log("Some data 1");
+        resolve("sucess");
+    }, 3000);
+    })
+}
+function asynFunc2(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+        console.log("Some data 2");
+        resolve("sucess");
+    }, 4000);
+    })
 }
 
-let sum = parseInt(a) + parseInt(b);
-console.log("The sum is :" , sum)
-// console.log("The sum is :" , sum*x) // here x is not defined so how to catch it //
+asynFunc1().then((res) => {
+    console.log("fetching data 1");
+    console.log(res);
+    asynFunc2().then((res2) => {
+    console.log("Fetching data 2");
+    console.log(res2);
+})
+})
 
-try {
-    console.log("The sum is:", sum*x);
-} catch (error) {
-    console.log("Error aa gaya")
-}
-
-// like we have function and we are retunning some value and we want to execute something after return statement //
-
-function main(){
-
-    let x = 2;
-    try {
-        console.log("The sum is:", sum*x);
-        return true;
-    } catch (error) {
-        console.log("Error aa gaya")
-        return false;
-    }
-    finally{
-        console.log("Executed !! ");
-    }
-}
-
-main();
