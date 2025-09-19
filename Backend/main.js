d1d59093b6c86ab7e8aff28888928587f3939781
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2025-09-19 11:21:55 +0530

    add

--- Backend/main.js
+++ Backend/main.js
@@ -4,4 +4,19 @@
 
 const fs = require("fs");
 
-console.log(fs); 
+// console.log(fs); 
+console.log("starting")
+
+fs.writeFile("gaur.txt" , "Gaur is an animal" ,() => {
+    console.log("done");
+
+    fs.readFile("gaur.txt", (error,data) => {
+        console.log(error,data.toString());
+    })
+})
+
+fs.appendFile("gaur.txt" , "Hello , how r u ?" , (e,d)=>{
+    console.log(d);
+})
+
+console.log("ending");
